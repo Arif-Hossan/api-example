bfc186cdb6e7eb681350ed62c3ad4485ec0d3e57
Arif Hossan <arif.hossan.eu@gmail.com>
2023-01-28 01:52:20 +0600

    fetch data from dynamic url

--- js/countries.js
+++ js/countries.js
@@ -9,15 +9,31 @@ const displayCounties = (countries) => {
   //     console.log(country);
   // }
   countries.forEach((country) => {
-    console.log(country);
+    // console.log(country);
     const countryDiv = document.createElement("div");
+    countryDiv.classList.add("country");
     countryDiv.innerHTML = `
-    <img style="min-width:2%;" src="${country.flags.png}">
+
     <h5>Name : ${country.name.common}</h5>
-    <P>Offical Name: ${country.name.official}</P>
+    <P>Official Name: ${country.name.official}</P>
     <P>Country : ${country.capital ? country.capital[0] : "No Capital"}</P>
-    `;
+    <button onClick="loadCountryDetail('${country.cca2}')"">Detail</button>`;
     countryContainer.appendChild(countryDiv);
   });
+};
+const loadCountryDetail = (code) => {
+  // https://restcountries.com/v3.1/alpha/{code}
+  const url = `https://restcountries.com/v3.1/alpha/${code}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data[0]));
+};
+const displayDetails = (country) => {
+  const detailsContainer = document.getElementById("details");
+  detailsContainer.innerHTML=`
+  <img style=" height:50px" src="${country.flags.png}">
+  <P>Country : ${country.capital ? country.capital[0] : "No Capital"}</P>
+  `;
 };
 loadCountries();
