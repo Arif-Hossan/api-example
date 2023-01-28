673f1acadb4aae19079eef6359a6e9cb76a446d8
Arif Hossan <arif.hossan.eu@gmail.com>
2023-01-29 01:12:00 +0600

    meal detals add

--- js/meals.js
+++ js/meals.js
@@ -8,14 +8,13 @@ const displayMeals = (meals) => {
   const mealContainer = document.getElementById("meal-container");
   mealContainer.innerHTML = ``;
   meals.forEach((meal) => {
-    console.log(meal);
+    // console.log(meal);
     const mealDiv = document.createElement("div");
-    mealDiv.classList.add("col");
+    // mealDiv.classList.add("col");
+    mealDiv.className = "col";
     mealDiv.innerHTML = `
-    <div class="card h-100">
-        <img src="${
-          meal.strMealThumb
-        }" class="card-img-top p-3" alt="..." />
+    <div onClick="loadMealDetail(${meal.idMeal})" class="card h-100">
+        <img src="${meal.strMealThumb}" class="card-img-top p-3" alt="..." />
         <div class="card-body">
           <h5 class="card-title">${meal.strMeal}</h5>
           <p class="card-text">
@@ -33,4 +32,27 @@ const searchFood = () => {
   loadMeals(searchText);
   searchField.value = "";
 };
-loadMeals("all");
+const loadMealDetail = (idMeal) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMealDetails(data.meals[0]));
+};
+
+const displayMealDetails = (meal) => {
+  const detailsContainer= document.getElementById('details-Container');
+  detailsContainer.style.display="block";
+  const mealsContainer = document.getElementById("meal-details");
+  mealsContainer.innerHTML=``;
+  const mealDiv = document.createElement("div");
+  mealDiv.classList.add("card");
+  mealDiv.innerHTML = `
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+    <div class="card-body">
+    <h5 class="card-title">${meal.strMeal}</h5>
+  <p class="card-text">
+    ${meal.strInstructions.slice(0,200)}
+  </p>`;
+  mealsContainer.appendChild(mealDiv);
+};
+loadMeals("");
